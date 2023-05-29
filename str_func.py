ab2a92fbd7838f2e7712a54ed026fc5b9ba3c63e
Andrey Sikorsky <nosb1992@gmail.com>
2023-05-29 23:52:09 +0300

    add new func adn docstring

--- str_func.py
+++ str_func.py
@@ -1,10 +1,10 @@
 def upper_str(a):
     """docsting"""
-    s = a.upper()
-    return s
+    new_upper_str = a.upper()
+    return new_upper_str
 
 
 def tittle_str(a):
     """docsting"""
-    s = a.title()
-    return s
+    new_title_str = a.title()
+    return new_title_str
